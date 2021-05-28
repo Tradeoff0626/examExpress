b92edd3777722d82c5752fd9b1bcff39acb41058
Kang, Kug-chin <tradeoff0626@gmail.com>
2021-05-29 00:50:54 +0900

    터미널 콘솔 로그(morgan) 및 '미들웨어' 적용 테스트 샘플

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const nunkuscks = require('nunjucks');  //npm install nunjucks
+const nunkuscks = require('nunjucks');  //npm install nunjucks [view engine]
+const logger = require('morgan');       //npm istall morgan [console logger <terminal>]
 
 const admin = require('./routes/admin');
 
 const app = express();
 const port = 3000;
+
+const vipMiddleware = ( req, res, next ) => {
+    console.log('최우선 미들웨어');
+    next();
+};
 
 //nunjucks 설정
 nunkuscks.configure('template' ,{   //'template'는 템플릿 경로
@@ -12,12 +18,16 @@ nunkuscks.configure('template' ,{   //'template'는 템플릿 경로
     express     : app               //express() 변수명
 });
 
+//미들웨어 설정
+app.use( logger('dev') );           //터미널 콘솔 로그 출력 
+
 app.get('/', (req, res) => {
     res.send('test express...');
 });
 
 //routing 등록(/admin) - admin 변수에 설정된 경로에 해당하는 js에 export된 router를 /admin 하위 경로로 등록
-app.use('/admin', admin);
+//app.use('/admin', admin);
+app.use('/admin', vipMiddleware, admin);        // '/admin' 및 하위에 모두 적용되는 최우선 미들웨어
 
 app.listen( port, () => {
     console.log(`Express listening on port `, port);
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 //routing 경로는 express.Router()에 추가 등록.
 
-router.get('/', (req, res) => {
+const testMiddlerWare1 = ( req, res, next ) => {
+    console.log('첫번째 미들웨어');
+    next();
+};
+
+const testMiddlerWare2 = ( req, res, next ) => {
+    console.log('두번째 미들웨어');
+    next();                                             //미들웨어 처리 후 계속 진행
+};
+
+//router.get('/', testMiddlerWare1, (req, res) => {
+router.get('/', testMiddlerWare1, testMiddlerWare2, (req, res) => {     //미들웨어 적용 테스트
     res.send('admin 이후 url');
 });
 
