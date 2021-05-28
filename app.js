d6eaf1bbf73d260e1cabe88f7155ce12cf66e04e
Kang, Kug-chin <tradeoff0626@gmail.com>
2021-05-28 22:30:43 +0900

    nunjucts [View engine] 적용 예시 / npm start 스크립트에 js,html 모니터링 추가

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const nunkuscks = require('nunjucks');  //npm install nunjucks
 
 const admin = require('./routes/admin');
 
 const app = express();
 const port = 3000;
+
+//nunjucks 설정
+nunkuscks.configure('template' ,{   //'template'는 템플릿 경로
+    autoescape  : true,             //템플릿의 html를 그대로 출력할지 여부 (XSS 공격 방지용)
+    express     : app               //express() 변수명
+});
 
 app.get('/', (req, res) => {
     res.send('test express...');
--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,10 @@ router.get('/', (req, res) => {
 });
 
 router.get('/products', (req, res) => {
-    res.send('admin products 이후 url');
+    //res.send('admin products 이후 url');
+    res.render( 'admin/products.html', {        //템플릿 렌더링. 파일의 경로는 app.js의 nunjucks 설정에서 입력한 템블릿 경로 이후 url
+        message : 'Hello!!'                     //템플릿에 전달할 값
+    } );
 });
 
 module.exports = router;
