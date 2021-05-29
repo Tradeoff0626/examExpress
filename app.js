0988c3cdabf14f207f31fce006e2864ce861b359
Kang, Kug-chin <tradeoff0626@gmail.com>
2021-05-29 14:53:45 +0900

    global view variable 적용 샘플

--- app.js
+++ app.js
@@ -24,7 +24,12 @@ app.use( logger('dev') );                                   //터미널 콘솔 �
 app.use( bodyParser.json() );                               //body-parser 초기화 설정
 app.use( bodyParser.urlencoded( { extended : false} ));     //body-parser 초기화 설정
 
-app.use( '/uploads', express.static('uploads') );           //정적파일 경로 설정 (/uploads 경로의 파일을 uploads 이름으로 접급)
+app.use( '/uploads', express.static('uploads') );           //정적파일 경로 설정 (/uploads 경로의 파일을 uploads 이름으로 접근)
+
+app.use( (req, res, next) => {
+    app.locals.isLogin = false;                              //global 변수 설정(test)
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('test express...');
