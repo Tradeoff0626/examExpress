1713c80e8ea1dca8002569c1d2446aca97604104
Kang, Kug-chin <tradeoff0626@gmail.com>
2021-05-30 01:10:03 +0900

    moment.js 적용하여 시간 포멧 설정 / 템플릿에서 모델에 필요한 함수 구현

--- models/Products.js
+++ models/Products.js
@@ -1,3 +1,5 @@
+const moment = require('moment');       //npm install moment
+
 module.exports = function(sequelize, DataTypes){
     const Products = sequelize.define('Products',
         {
@@ -7,5 +9,11 @@ module.exports = function(sequelize, DataTypes){
             description : { type: DataTypes.TEXT }
         }
     );
+
+    //템플릿에서 함수 형식으로 호출 처리. 모델에서 함수 처리가 필요한 경우 이와 같이 활용 가능
+    Products.prototype.dateFormat = (date) => (
+        moment(date).format('YYYY-MM-DD')       //moment(date).format('YYYY년 MM월 DD일')
+    );
+
     return Products;
 }
