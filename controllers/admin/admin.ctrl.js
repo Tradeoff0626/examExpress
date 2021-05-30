b813fbc4ffd6af986e237ec07d2e8002c69d4ae4
Kang, Kug-chin <tradeoff0626@gmail.com>
2021-05-30 11:42:08 +0900

    DB 처리 async/await로 수정

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -14,29 +14,32 @@ const models = require('../../models');
 
 
 //전체 목록 조회
-exports.get_products = ( _ , res) => {
+exports.get_products = async ( _ , res) => {
     //res.send('admin products 이후 url');
     
     //res.render( 'admin/products.html', {        //템플릿 렌더링. 파일의 경로는 app.js의 nunjucks 설정에서 입력한 템블릿 경로 이후 url
     //    message : '<h1>Hello!!!</h1>'           //2번째 인자. 템플릿에 전달할 값 <K-V>.
     //} );
 
-    models.Products.findAll({
-
-    }).then( (products) => {
-        // DB에서 받은 products를 products변수명으로 내보냄
-        res.render( 'admin/products.html', { products : products });    //res.render( 'admin/products.html', { products });
-    });
+    try {
+        const products = await models.Products.findAll();  
+        res.render( 'admin/products.html', { products });
+    } catch(e) {
+        console.log(e);
+    }
 
 }
 
 
 //단건 조회(상세보기)
-exports.get_products_detail = ( req , res ) => {
+exports.get_products_detail = async ( req , res ) => {
 
-    models.Products.findByPk(req.params.id).then( (product) => {
-        res.render('admin/detail.html', { product : product });         //res.render( 'admin/products.html', { product });
-    });
+    try {
+        const product = await models.Products.findByPk(req.params.id);
+        res.render('admin/detail.html', { product });         //res.render( 'admin/products.html', { product : product });
+    } catch(e) {
+        console.log(e);
+    }
 
 };
 
@@ -47,66 +50,84 @@ exports.get_products_write = ( _ , res) => {
 
 
 //제품 입력
-exports.post_products_write = ( req , res ) => {
+exports.post_products_write = async ( req , res ) => {
     //res.send('post send test...');
     //res.send(req.body.price);             //템플릿 (write.html)에 name이 price인 속성의 input값 전달
     //res.send(req.body);                   //템플릿 (write.html)에 name 속성이 설정된 모든 input값 전달
 
 
-    /*
-    //전체 일괄 입력(req.body와 모델의 입력 컬림이 완전히 동일한 경우)
-    models.Products.create(req.body).then( () => {
+    try {        
+        //전체 일괄 입력(req.body와 모델의 입력 컬림이 완전히 동일한 경우)
+        await models.Products.create(req.body);
         res.redirect('/admin/products');
-    });
-    */
 
-    //컬럼 개별 입력
-    models.Products.create({
-        name : req.body.name,
-        price : req.body.price ,
-        description : req.body.description
-    }).then( () => {
+        /*
+        //컬럼 개별 입력
+        await models.Products.create({
+            name : req.body.name,
+            price : req.body.price ,
+            description : req.body.description
+        });
+        */
+
         res.redirect('/admin/products');
-    });
+    } catch(e) {
+        console/log(e);
+    }
 
 }
 
 
 //제품 수정할 데이터 조회
-exports.get_products_edit = ( req , res ) => {
-    //기존에 폼에 value안에 값을 셋팅하기 위해 만든다.
-    models.Products.findByPk(req.params.id).then( (product) => {
-        res.render('admin/write.html', { product : product });
-    });
+exports.get_products_edit = async ( req , res ) => {
+
+    try {
+        //기존에 폼에 value안에 값을 셋팅하기 위해 조회
+        const product = await models.Products.findByPk(req.params.id);
+        res.render('admin/write.html', { product });
+    } catch(e) {
+        console.log(e);
+    }
 };
 
 
 //제품 수정 데이터 전달
-exports.post_products_edit = ( req , res ) => {
+exports.post_products_edit = async ( req , res ) => {
 
-    models.Products.update(
-        {                                           //수정할 데이터
-            name : req.body.name,
-            price : req.body.price ,
-            description : req.body.description
-        }, 
-        {                                           //수정 조건(where)
-            where : { id: req.params.id } 
-        }
-    ).then( () => {
+    try {
+        await models.Products.update(
+            /*
+            {                                           //수정할 데이터
+                name : req.body.name,
+                price : req.body.price ,
+                description : req.body.description
+            }*/
+            req.body, 
+            {                                           //수정 조건(where)
+                where : { id: req.params.id } 
+            }
+        );
+
         res.redirect('/admin/products/detail/' + req.params.id );       //이전 상세 페이지로 이동
-    });
+    } catch(e) {
+        console.log(e);
+    }
 
 };
 
 
 //제품 삭제(단건)
-exports.get_products_delete = ( req , res ) => {
-    models.Products.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then( () => {
+exports.get_products_delete = async ( req , res ) => {
+    
+    try {
+        await models.Products.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+
         res.redirect('/admin/products');
-    });
+    } catch(e) {
+        console.log(e);
+    }
 };
